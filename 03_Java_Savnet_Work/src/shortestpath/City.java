1a835ae4a4e5516c0cf132f2f1292b3cc8e465bd
alinr <alinr@192.168.0.100>
2019-07-18 00:04:59 +0300

    Shortest Path

---
+++ 03_Java_Savnet_Work/src/shortestpath/City.java
@@ -0,0 +1,56 @@
+package shortestpath;
+
+import java.util.HashMap;
+
+public class City {
+	private String name;
+	private HashMap<City, Integer> neighbours = new HashMap<>();
+
+	public City(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void addNeighbour(City city, int distance) {
+		neighbours.put(city, distance);
+	}
+
+	public HashMap<City, Integer> getNeighbours() {
+		return neighbours;
+	}
+
+	@Override
+	public String toString() {
+		return " " + name + " ";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
---
+++ 03_Java_Savnet_Work/src/shortestpath/Route.java
@@ -0,0 +1,32 @@
+package shortestpath;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route {
+	List<City> route = new ArrayList<>();
+
+	public void add(City city) {
+		route.add(city);
+	}
+
+	public void show() {
+		for (City city : route) {
+			System.out.print(city);
+		}
+	}
+
+	boolean contains(City start) {
+		for (City city : route) {
+			if (city.equals(start)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void removeLast() {
+		route.remove(route.size() - 1);
+	}
+
+}
