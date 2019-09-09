e3e8eab0c9938db14a6c59100b6e808acccf8ddf
alinr <alinr@192.168.0.108>
2019-09-09 20:33:15 +0300

    up

---
+++ 02_Recursion_exercises/src/shortestpath/app/Application.java
@@ -0,0 +1,24 @@
+package shortestpath.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Application app = new Application();
+		app.run();
+	}
+
+	private void run() {
+		CityRoutes cr = new CityRoutes();
+		cr.init();
+		List<Route> routes = new ArrayList<>();
+		routes = cr.findRoutes("Timisoara", "Deva");
+		for (Route route : routes) {
+			route.show();
+		}
+
+	}
+
+}
