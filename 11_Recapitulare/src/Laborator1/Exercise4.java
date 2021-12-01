483010eeb4e05e110de3342c10303c1c81ef6222
uidr4989 <uidr4989@TMLN235W.cw01.contiwan.com>
2021-12-01 17:54:00 +0200

    Exercise4 done!

--- 11_Recapitulare/src/Laborator1/Exercise4.java
+++ 11_Recapitulare/src/Laborator1/Exercise4.java
@@ -7,17 +7,41 @@ public class Exercise4 {
 
 	public static void main(String[] args) {
 		Keyboard keyboard = new Keyboard();
-		ArrayList<Integer> primeNrList = new ArrayList<Integer>();
+
 		Random random = new Random();
-		int maximumRange = keyboard.getInt("Introduceti numarul maxim blb bla.");
+		int maximumRange;
+		int a, b, cmmdc;
 
-		primeNrList = generatePrimeList(maximumRange);
-
-		int a, b;
+		maximumRange = keyboard.getInt("Introduceti numarul maxim blb bla.");
 		a = random.nextInt(maximumRange);
 		b = random.nextInt(maximumRange);
 
-		System.out.println("Lista numerelor prime pana la numarul " + maximumRange + " este : " + primeNrList);
+		System.out.println("Primul numar generat aleator : " + a);
+		System.out.println("Al 2-lea numar generat aleator : " + b);
+
+		cmmdc = getCmmdc(a, b);
+		if (cmmdc == 1) {
+			System.out.println("Numerele " + a + " si " + b + " sunt prime intre ele.");
+		} else {
+			System.out.println("Cel mai mare divizor comun al numereleor " + a + " si " + b + " este : " + cmmdc);
+		}
+
+	}
+
+	private static int getCmmdc(int a, int b) {
+		if (a >= b) {
+			return cmmdc(a, b);
+		} else {
+			return cmmdc(b, a);
+		}
+	}
+
+	private static int cmmdc(int a, int b) {
+		if (a % b == 0) {
+			return b;
+		} else {
+			return getCmmdc(b, a % b);
+		}
 	}
 
 	private static ArrayList<Integer> generatePrimeList(int upToNumber) {
