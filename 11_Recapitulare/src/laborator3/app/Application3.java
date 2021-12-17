cb4d1acea09ec325e0ef3cb5ea63dde89d70eba7
uidr4989 <uidr4989@TMLN235W.cw01.contiwan.com>
2021-12-17 13:32:43 +0200

    Application 3 done!

--- 11_Recapitulare/src/laborator3/app/Application3.java
+++ 11_Recapitulare/src/laborator3/app/Application3.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import inputOotputClasses.FileReaderWriter;
+import inputOotputClasses.Keyboard;
 
 public class Application3 {
 	public static void main(String[] args) {
 		ArrayList<String> judete;
+		String cautaJudet;
+		int indexOfCautaJudet;
+
 		try {
 			FileReaderWriter file = new FileReaderWriter("judete_in.txt");
 			judete = file.extractor();
@@ -16,9 +20,34 @@ public class Application3 {
 
 			viewList(judete);
 
+			indexOfDistrict(judete);
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	@SuppressWarnings("static-access")
+	private static void indexOfDistrict(ArrayList<String> judete) {
+		Keyboard keyboard = new Keyboard();
+		String cautaJudet;
+		boolean flag = true;
+
+		do {
+			System.out.println("\nPlease enter a valid District!");
+			cautaJudet = keyboard.getMessage("\nInsert District name: ");
+			int indexOf;
+//			boolean isMatched = judete.stream().anyMatch(cautaJudet::equalsIgnoreCase);
+
+			for (indexOf = 0; indexOf < judete.size(); indexOf++) {
+				if (cautaJudet.equalsIgnoreCase(judete.get(indexOf))) {
+					System.out.println("Judetul cautat este pe pozitia: " + indexOf);
+					flag = false;
+					break;
+				}
+			}
+		} while (flag);
 
 	}
 
